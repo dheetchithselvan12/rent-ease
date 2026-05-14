4f4848fba3f4ebdfce4dd9306fa914b5638ab102
DHEETCHITHSELVAN A <dheetchithselvan12@gmail.com>
2026-05-14 16:48:03 +0530

    Created: AllProducts page only layout of the page and productDetaile page added similar products
    conflict resolved

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import { Routes, Route } from "react-router-dom";
 import ProductDetailes from "./pages/ProductDetailes";
+import AllProducts from "./pages/AllProducts";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/products/:id" element={<ProductDetailes />} />
+        <Route path="/products" element={<AllProducts />} />
       </Routes>
       <Footer />
     </>
