0ab021e7c352e5cc65b121d72b995d7a89c89fa7
dheetchithselvan12 <dheetchithselvan12@gmail.com>
2026-05-21 17:15:01 +0530

    Cart: updated many: product schema, removed delivery form updated with similar items, and Allproduct page updated

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -1,14 +1,15 @@
-import { useState } from "react";
+// import { useState } from "react";
 import { useSelector } from "react-redux";
 import { removeFromCart } from "../features/cart/cartSlice";
 import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
-import CartCard from "../components/cart/CartCard";
+import CartCard from "../components/cart/CartItems";
 import CartPayment from "../components/cart/CartPayment";
-import {
-  DeliveryDetailesForm,
-  DeliveryScheduleForm,
-} from "../components/cart/DeliveryDetailesForm";
+import SimilarCartItems from "../components/cart/SimilarCartItems";
+// import {
+//   DeliveryDetailesForm,
+//   DeliveryScheduleForm,
+// } from "../components/cart/DeliveryDetailesForm";
 
 const Cart = () => {
   const { cartItem } = useSelector((state) => state.cart);
@@ -17,20 +18,20 @@ const Cart = () => {
   console.log("CartIems: ", cartItem);
 
   // Lifted state for delivery forms
-  const [deliveryDetails, setDeliveryDetails] = useState({
-    name: "",
-    phone: "",
-    address: "",
-    city: "",
-    state: "",
-    zipCode: "",
-  });
-  const [deliverySchedule, setDeliverySchedule] = useState({
-    deliveryDate: "",
-    preferredTime: "",
-  });
+  // const [deliveryDetails, setDeliveryDetails] = useState({
+  //   name: "",
+  //   phone: "",
+  //   address: "",
+  //   city: "",
+  //   state: "",
+  //   zipCode: "",
+  // });
+  // const [deliverySchedule, setDeliverySchedule] = useState({
+  //   deliveryDate: "",
+  //   preferredTime: "",
+  // });
 
-  console.log("DeliveryDetailesForm: ", deliveryDetails);
+  // console.log("DeliveryDetailesForm: ", deliveryDetails);
 
   const handleRemoveCart = (productId) => {
     dispatch(removeFromCart(productId));
@@ -40,7 +41,7 @@ const Cart = () => {
   return (
     <div className="px-15 py-10 bg-gray-100 min-h-screen">
       <div className="mb-8">
-        <h2 className="text-3xl font-medium text-gray-800">Checkout</h2>
+        <h2 className="text-3xl font-medium text-gray-800">Your Cart</h2>
         <p className="text-gray-500 mt-2 text-lg">
           Review your rental items and complete your order.
         </p>
@@ -51,7 +52,7 @@ const Cart = () => {
           <div className="border border-gray-200 rounded-xl p-6 bg-white shadow-sm">
             {/* cart */}
             <h3 className="text-xl font-semibold text-gray-800 mb-6">
-              Your Cart
+              Cart Items
             </h3>
             {cartItem?.length === 0 ? (
               <div className="text-center text-gray-500 py-10">
@@ -78,7 +79,8 @@ const Cart = () => {
 
           {/* bottom */}
           <div className="flex flex-col gap-6 h-fit">
-            <DeliveryDetailesForm
+            <SimilarCartItems />
+            {/* <DeliveryDetailesForm
               formData={deliveryDetails}
               handleChange={(e) =>
                 setDeliveryDetails((prev) => ({
@@ -95,14 +97,14 @@ const Cart = () => {
                   [e.target.name]: e.target.value,
                 }))
               }
-            />
+            /> */}
           </div>
         </div>
 
         {/* right side */}
         <CartPayment
-          deliveryDetails={deliveryDetails}
-          deliverySchedule={deliverySchedule}
+        // deliveryDetails={deliveryDetails}
+        // deliverySchedule={deliverySchedule}
         />
       </section>
     </div>
