8d9d9f8e411b3bfe054e86d081608882fc868735
dheetchithselvan12 <dheetchithselvan12@gmail.com>
2026-06-30 17:19:37 +0530

    user login data stored in local storage using redux persist

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -3,6 +3,24 @@ import productReducer from "../features/products/productsSlice.js";
 import cartReducer from "../features/cart/cartSlice.js";
 import checkoutReducer from "../features/checkout/checkoutSlice.js";
 import orderReducer from "../features/order/orderSlice.js";
+import { 
+    persistReducer, persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER 
+} from 'redux-persist';
+import storage from 'redux-persist/es/storage';
+import authReducer from "../features/auth/authSlice.jsx";
+
+const persistConfig = {
+    key: 'auth',
+    storage,
+    whitelist: ['user', 'token'],
+}
+const persistedReducer = persistReducer(persistConfig, authReducer);
 
 export const store = configureStore({
     reducer: {
@@ -10,6 +28,13 @@ export const store = configureStore({
         cart: cartReducer,
         checkout: checkoutReducer,
         orders: orderReducer,
-    
+        auth: persistedReducer,
     },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+        serializableCheck: {
+            ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+        },
+    }), 
 });
+
+export const persistor = persistStore(store);
